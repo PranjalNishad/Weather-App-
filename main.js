1a604e94042be64595884b7e194c25bfcadee843
Pranjal Nishad <nishadpranjal450@gmail.com>
2025-08-13 19:32:34 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,46 @@
+const url ='https://api.openweathermap.org/data/2.5/weather';
+const apiKey ='f00c38e0279b7bc85480c3fe775d518c';
+
+// initial calling default location
+
+$(document).ready(function () {
+	weatherFn('Pune');
+});
+
+// fetching weather details
+
+async function weatherFn(cName) {
+	const temp =`${url}?q=${cName}&appid=${apiKey}&units=metric`;
+
+// fetch request to api , converts api response to json format (light weight data change format which is easy to read for humans)
+
+	try {
+		const res = await fetch(temp);
+		const data = await res.json();
+		if (res.ok) {
+			weatherShowFn(data);
+		} else {
+			alert('City not found. Please try again.');
+		}
+	} catch (error) {
+		console.error('Error fetching weather data:', error);
+	}
+}
+
+// displaying weather data , moment library
+
+function weatherShowFn(data) {
+	$('#city-name').text(data.name);
+	$('#date').text(moment().
+		format('MMMM Do YYYY, h:mm:ss a'));
+	$('#temperature').
+		html(`${data.main.temp}°C`);
+	$('#description').
+		text(data.weather[0].description);
+	$('#wind-speed').
+		html(`Wind Speed: ${data.wind.speed} m/s`);
+	$('#weather-icon').
+		attr('src',
+			`...`);
+	$('#weather-info').fadeIn();
+}
